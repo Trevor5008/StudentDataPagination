492aa5841d5200313587318c4c815691ff4ea114
Trevor <trevorwerner@hotmail.com>
2021-07-27 06:01:03 -0400

    Added search bar functionality, comments, and a basic readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# StudentDataPagination - Treehouse project #2
+
+## In this simple application, student data is presented in paginated format with the added functionality of a search bar for filtering results.
+
+# Trevor Werner / July 27th, 2021
--- js/script.js
+++ js/script.js
@@ -3,50 +3,53 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+const header = document.querySelector('header');
+const linkList = document.querySelector('ul.link-list');
 
+const addSearchBar = () => {
+   let searchBar = document.createElement('div');
+   searchBar.innerHTML = `
+      <label for="search" class="student-search">
+         <span>Search by name</span>
+         <input id="search" placeholder="Search by name...">
+         <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+      </label>
+   `;
+   header.append(searchBar);
+};
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
 const showPage = (list, page) => {
    const startIdx = (page * 9) - 9;
    const endIdx = page * 9;
    const studentList = document.querySelector('ul.student-list');
    studentList.innerHTML = '';
    let html = '';
-   for (let i = startIdx; i < list.length; i++) {
-      if (i >= startIdx && i < endIdx) {
-         html += `  
-            <li class="student-item cf">
-               <div class="student-details">
-                  <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
-                  <h3>${list[i].name.first} ${list[i].name.last}</h3>
-                  <span class="email">${list[i].email}</span>
-               </div>
-               <div class="joined-details">
-                  <span class="date">Joined ${list[i].registered.date}</span>
-               </div>
-            </li>`;
+   // display message if zero search results
+   if (list.length === 0) {
+      html += `<h2 style="text-align: center">Sorry, no matching results</h2>`;
+   } else {
+      for (let i = startIdx; i < list.length; i++) {
+         if (i >= startIdx && i < endIdx) {
+            html += `  
+               <li class="student-item cf">
+                  <div class="student-details">
+                     <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+                     <h3>${list[i].name.first} ${list[i].name.last}</h3>
+                     <span class="email">${list[i].email}</span>
+                  </div>
+                  <div class="joined-details">
+                     <span class="date">Joined ${list[i].registered.date}</span>
+                  </div>
+               </li>`;
+         }
       }
    }
    studentList.innerHTML = html;
 }
 
-
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
-
+// adds the page selection buttons to bottom of page
 const addPagination = (list) => {
    const pages = Math.ceil(list.length / 9);
-   const linkList = document.querySelector('ul.link-list');
    linkList.innerHTML = '';
    let html = '';
    for (let i = 1; i <= pages; i++) {
@@ -59,14 +62,29 @@ const addPagination = (list) => {
    linkList.innerHTML = html;
 };
 
-const pageBtns = document.getElementsByTagName('button');
 // initial load on page 1
 let currentPage = 1;
 
 // Call functions
+addSearchBar();
 showPage(data, currentPage);
 addPagination(data);
 
+const pageBtns = linkList.querySelectorAll('li button');
+const searchFld = document.getElementById('search');
+const searchBtn = searchFld.nextElementSibling;
+
+searchBtn.addEventListener('click', () => {
+   const searchTerm = searchFld.value.toUpperCase();
+   searchFld.value = '';
+   // if search field is empty, show all data
+   let filterData = searchTerm !== '' ? data.filter(student => student.name.first.toUpperCase() === searchTerm) : data;
+
+   showPage(filterData, 1);
+   addPagination(filterData);
+});
+
+// attach event handler to each page button
 for (let i = 0; i < pageBtns.length; i++) {
    pageBtns[i].addEventListener('click', (e) => {
       const button = e.target;
@@ -77,5 +95,5 @@ for (let i = 0; i < pageBtns.length; i++) {
       showPage(data, currentPage);
       // set active class on current target
       button.className = 'active';
-   })
+   });
 }
